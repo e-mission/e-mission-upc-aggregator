976cd56b9f6c5b9958bd5340f97c98fd96a420f4
Nicolas <njriasan@ante.CS.Berkeley.EDU>
2019-05-10 20:57:30 -0700

    added a launch machine script

---
+++ launch_machine.py
@@ -0,0 +1,16 @@
+import socket
+import subprocess
+import sys
+from emission.net.int_service.machine_configs import controller_port, controller_ip
+
+cloudVarName = "PORTMAP"
+
+def main():
+    if "http://" + socket.gethostbyname(socket.gethostname()) == controller_ip:
+        envVars = {cloudVarName: "{}:{}".format (controller_port, controller_port)}
+        ret = subprocess.Popen (["docker-compose", "-f", "docker/docker-compose-controller.yml", "up", "-d"], cwd="./", env=envVars)
+        ret.wait ()
+    subprocess.Popen (["./e-mission-py.bash", "emission/net/int_service/swarm.py"], cwd="./", stdout=f, stderr=f)
+
+if __name__ == "__main__":
+    main ()
