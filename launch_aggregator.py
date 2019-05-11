169ca0bb39c8d0e3beb47554bdf0ad203b6feac9
Nicolas <njriasan@ante.CS.Berkeley.EDU>
2019-05-10 21:24:34 -0700

    moved launch aggregator to support an upper bound

--- launch_aggregator.py
+++ launch_aggregator.py
@@ -9,16 +9,20 @@ username = "test_analyst"
 # Default location of the query.
 query_file = "query.json"
 
-def main ():
+def main (upperbound):
     r = requests.post (controller_addr + register_user_endpoint, json={'user':username})
     with open (query_file, "r") as f:
         query = json.load (f)
         print(query)
     if r.ok:
-        subprocess.run (["python", "emission/net/ext_service/aggregator.py", controller_addr, "4", username, "test-querier"])
+        subprocess.run (["./e-mission-py.bash", "emission/net/ext_service/aggregator.py", controller_addr, "1", upperbound, username, "test-querier"])
     else:
         print ("Error when registering the user.", file=sys.stderr)
         sys.exit (1)
 
 if __name__ == "__main__":
-    main ()
+    parser = argparse.ArgumentParser (description="Script to generate the query request")
+    parser.add_argument ("upper_bound", type=int,
+            help="Maximum number of users to request")
+    items = parser.parse_args ()
+    main (str (items.upper_bound))
