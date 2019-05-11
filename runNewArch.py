99976169b89b2db9534da58ff8405c25b3a7e8f3
Nicolas <njriasan@ante.CS.Berkeley.EDU>
2019-05-11 14:58:48 -0700

    fixed a couple issues

--- runNewArch.py
+++ runNewArch.py
@@ -8,22 +8,21 @@ controller_addr = "{}:{}".format (controller_ip, controller_port)
 
 def main ():
     # First we want to spawn a the controller in a new shell
-    #num_users = [10, 50, 100, 500, 1000]
-    num_users = [2, 5]
-    num_trips = [2, 5]
-    #num_trips = [10, 50, 100, 500, 1000]
-    num_queries = 5
+    num_users = [10, 50, 100, 500, 1000]
+    num_trips = [10, 50, 100, 500, 1000]
+    num_queries = 40
     for curr_num_users in num_users:
         for curr_num_trips in num_trips:
             ret = subprocess.Popen (["./e-mission-py.bash", "emission/simulation/simulate_fake_users.py", str(curr_num_users), str(curr_num_trips)], cwd="./")
             ret.wait ()
-
-            for _ in range(num_queries):
-                requests.post (controller_addr + "/kill_all_queriers")
-                requests.post (controller_addr + "/pause_all_clouds")
-                csv_file_name = "csvs/time_" + str(curr_num_users) + "_" + str(curr_num_trips) + ".csv"
-                ret = subprocess.Popen (["./e-mission-py.bash", "launch_aggregator.py", csv_file_name, str (curr_num_users)], cwd="./")
-                ret.wait ()
+            for i in range (1, 11, 1):
+                i = i * .1
+                for _ in range(num_queries):
+                    requests.post (controller_addr + "/kill_all_queriers")
+                    requests.post (controller_addr + "/pause_all_clouds")
+                    csv_file_name = "csvs/time_" + str(curr_num_users) + "_" + str(curr_num_trips) + "_" + str (i * 100) + ".csv"
+                    ret = subprocess.Popen (["./e-mission-py.bash", "launch_aggregator.py", csv_file_name, str (curr_num_users * i)], cwd="./")
+                    ret.wait ()
             requests.post (controller_addr + "/clear_containers")
 
 
