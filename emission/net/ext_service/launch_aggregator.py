3ad8cc939a4e1e539ac4f9c4652e5028dd5f9fdb
Jack Sullivan <jsullivan27@berkeley.edu>
2019-05-11 17:27:36 -0700

    set up query correctness experiment

--- emission/net/ext_service/launch_aggregator.py
+++ emission/net/ext_service/launch_aggregator.py
@@ -1,5 +1,6 @@
 import subprocess, sys, requests
 import json
+import argparse
 from emission.net.int_service.machine_configs import controller_ip, controller_port, register_user_endpoint
 
 
@@ -7,21 +8,26 @@ controller_addr = "{}:{}".format (controller_ip, controller_port)
 username = "test_analyst"
 
 # Default location of the query.
-query_file = "query.json"
+# query_file = "query.json"
 
-def main (csv_file):
+def main (query_file, csv_file, upperbound):
     r = requests.post (controller_addr + register_user_endpoint, json={'user':username})
     with open (query_file, "r") as f:
         query = json.load (f)
-        print(query)
     if r.ok:
-        subprocess.run (["python", "emission/net/ext_service/aggregator.py", controller_addr, "4", username, "test-querier", csv_file])
-
-        # Put timer here, gets the time of launching the aggregator.
+        ret = subprocess.Popen (["./e-mission-py.bash", "emission/net/ext_service/aggregator.py", query, controller_addr, "1", upperbound, username, "test-querier", csv_file])
+        ret.wait ()
     else:
         print ("Error when registering the user.", file=sys.stderr)
         sys.exit (1)
 
 if __name__ == "__main__":
-    csv_file = sys.argv[1]
-    main (csv_file)
+    parser = argparse.ArgumentParser (description="Script to generate the query request")
+    parser.add_argument ("query_file", type=str,
+            help="Query to conduct")
+    parser.add_argument ("csv_name", type=str,
+            help="Unique name to CSV file based on time")
+    parser.add_argument ("upper_bound", type=int,
+            help="Maximum number of users to request")
+    items = parser.parse_args ()
+    main (items.query_file, items.csv_name, str (items.upper_bound))
