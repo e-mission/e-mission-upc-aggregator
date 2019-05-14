c6b287a07f933e989b4e3c96186b39477c2bd7e3
Jack Sullivan <jsullivan@c79.Millennium.Berkeley.EDU>
2019-05-14 11:55:46 -0700

    changed config

--- runNewArch.py
+++ runNewArch.py
@@ -9,9 +9,9 @@ controller_addr = "{}:{}".format (controller_ip, controller_port)
 def main ():
     query_file = "query.json"
     subprocess.Popen (["mkdir", "-p", "csvs"], cwd="./")
-    num_users = [100, 500, 1000]
+    num_users = [3, 500, 1000]
     num_trips = [10, 50, 100]
-    num_queries = 40
+    num_queries = 30
     for curr_num_users in num_users:
         for curr_num_trips in num_trips:
             requests.post (controller_addr + "/setup_networks")
