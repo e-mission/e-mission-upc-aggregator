842db36dd5f4a1fa34f8fdf814bd77eb16ab0994
Jack Sullivan <jsullivan27@berkeley.edu>
2019-05-15 01:50:29 -0700

    updated correctness exps

--- runUserPolicy.py
+++ runUserPolicy.py
@@ -20,17 +20,19 @@ def main ():
     num_users = 20
     num_trips = 5
     num_queries = 100
+    requests.post (controller_addr + "/setup_networks")
     ret = subprocess.Popen (["./e-mission-py.bash", "emission/simulation/simulate_fake_users.py", str(num_users), str(num_trips)], cwd="./")
     ret.wait ()
     for query_file in query_files:
         csv_file_name = "csvs/policy_all_correctness_" + query_file + "_0.05_" + str(num_users) + "_" + str(num_trips) + ".csv"
         for _ in range(num_queries):
-            requests.post (controller_addr + "/kill_all_queriers")
+            requests.post (controller_addr + "/pause_all_queriers")
             requests.post (controller_addr + "/pause_all_clouds")
             ret = subprocess.Popen (["./e-mission-py.bash", "emission/net/ext_service/launch_aggregator.py", query_file, csv_file_name, str(num_users)], cwd="./")
             ret.wait ()
     requests.post (controller_addr + "/clear_containers")
 
+    requests.post (controller_addr + "/setup_networks")
     ret = subprocess.Popen (["./e-mission-py.bash", "emission/simulation/simulate_fake_users.py", str(num_users / 2), str(num_trips)], cwd="./")
     ret.wait ()
     wipe_agg_json("emission/simulation/known_aggs.json")
@@ -39,7 +41,7 @@ def main ():
     for query_file in query_files:
         csv_file_name = "csvs/policy_half_correctness_" + query_file + "_0.05_" + str(num_users) + "_" + str(num_trips) + ".csv"
         for _ in range(num_queries):
-            requests.post (controller_addr + "/kill_all_queriers")
+            requests.post (controller_addr + "/pause_all_queriers")
             requests.post (controller_addr + "/pause_all_clouds")
             ret = subprocess.Popen (["./e-mission-py.bash", "emission/net/ext_service/launch_aggregator.py", query_file, csv_file_name, str(num_users)], cwd="./")
             ret.wait ()
