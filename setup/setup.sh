75e2cf8154021eefc86f2f0928fcbab619901c68
njriasan <njriasanovsky@berkeley.edu>
2020-04-15 10:03:00 -0700

    trying another setup fix

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,7 +7,6 @@
 
 echo "Setting up blank environment"
 conda create --name emission python=3.6
-source activate emission
 
 echo "Downloading packages"
 curl -o /tmp/cachetools-2.1.0-py_0.tar.bz2 -L https://anaconda.org/conda-forge/cachetools/2.1.0/download/noarch/cachetools-2.1.0-py_0.tar.bz2
@@ -22,3 +21,5 @@ conda install /tmp/*.bz2
 
 echo "Updating using conda now"
 conda env update --name emission --file setup/environment36.yml
+
+source activate emission
