c1056991fd50d72f6b952f7a5be0b83527e00f71
njriasan <njriasanovsky@berkeley.edu>
2020-04-15 22:55:21 -0700

    reduced the code talking to the pm

--- emission/net/int_service/machine_configs.py
+++ emission/net/int_service/machine_configs.py
@@ -16,14 +16,10 @@ for key, value in machines_dict.items ():
     machines_list.append ((key, float (value)))
 
 register_user_endpoint = config_data["register_user_endpoint"]
-user_cache_endpoint = config_data["user_cache_endpoint"]
+store_endpoint = config_data["store_endpoint"]
 service_endpoint = config_data["service_endpoint"]
-cloud_status_endpoint = config_data["cloud_status_endpoint"]
 cloud_key_endpoint = config_data["cloud_key_endpoint"]
-cloud_profile_endpoint = config_data["cloud_profile_endpoint"]
 cloud_aggregate_endpoint = config_data["cloud_aggregate_endpoint"]
-query_endpoint = config_data["query_endpoint"]
-get_users_endpoint = config_data["get_users_endpoint"]
 query_endpoint = config_data["query_endpoint"]
 certificate_bundle_path = config_data["certificate_bundle"]
 
