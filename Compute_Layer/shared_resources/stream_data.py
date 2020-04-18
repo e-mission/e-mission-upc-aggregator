a1ae05373bd035bbd2202a19e228a42c3083045f
njriasan <njriasanovsky@berkeley.edu>
2020-04-18 01:08:07 -0700

    added generic stream data functions

--- Compute_Layer/shared_resources/stream_data.py
+++ Compute_Layer/shared_resources/stream_data.py
@@ -7,12 +7,15 @@ def get_calendar_keys():
     keys_dict[key_one] =[["ASCENDING", "ASCENDING", "ASCENDING", "ASCENDING", "ASCENDING", "GEOSPHERE"], "False"]
     return keys_dict
 
-def store_calendar_data(target_address, data, certificate_path):
+def store_calendar_data(target_address, certificate_path, data):
+    store_data(target_address, "Stage_calendar", get_calendar_keys(), data, certificate_path)
+
+def store_data(target_address, certificate_path, data_type, keys, data):
     error = False
     try:
         json_entries = dict()
-        json_entries['data_type'] = "Stage_calendar"
-        json_entries['keys'] = get_calendar_keys()
+        json_entries['data_type'] = data_type
+        json_entries['keys'] = keys
         json_entries['data'] = data
         r = requests.post(target_address, json=json_entries, timeout=300, verify=certificate_path)
     except (socket.timeout) as e:
@@ -22,29 +25,33 @@ def store_calendar_data(target_address, data, certificate_path):
     if not r.ok or error:
         error = True
     if error:
-        print('Something went wrong when trying to sync your calendar data.')
+        print('Something went wrong when trying to sync your {} data.'.format(data_type))
         print(r.content)
     else:
-        print("calendar data sucessfully synced to the server")
+        print("{} data sucessfully synced to the server".format(data_type))
 
-def load_calendar_data(target_address, data, certificate_path):
+
+def load_calendar_data(target_address, certificate_path, search_fields, should_sort=False, sort=None):
+    return load_data(target_address, certificate_path, "Stage_calendar", get_calendar_keys(), search_fields, should_sort, sort)
+
+def load_data(target_address, certificate_path, data_type, keys, search_fields, should_sort=False, sort=None):
     error = False
     try:
         json_entries = dict()
-        json_entries['data_type'] = "Stage_calendar"
-        json_entries['keys'] = get_calendar_keys()
-        json_entries['search_fields'] = [{"metadata.type": "calendar"}, {"_id": "False"}]
-        json_entries['should_sort'] = "True"
-        json_entries['sort'] = {'data.end_time': "False"}
+        json_entries['data_type'] = data_type
+        json_entries['keys'] = keys
+        json_entries['search_fields'] = search_fields
+        if should_sort:
+            json_entries['should_sort'] = "True"
+            json_entries['sort'] = sort
+        else:
+            json_entries['should_sort'] = "False"
         r = requests.post(target_address, json=json_entries, timeout=300, verify=certificate_path)
-    except (socket.timeout) as e:
-        error = True
-
     #Check if sucessful
     if not r.ok or error:
         error = True
     if error:
-        print('Something went wrong when trying to load your calendar data.')
+        print('Something went wrong when trying to load your {} data.'.format(data_type))
         print(r.content)
     else:
         return r.text
