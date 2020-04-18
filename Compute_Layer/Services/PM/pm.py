74cec2fc5dec5439813724f68e4bf8ef27148663
njriasan <njriasanovsky@berkeley.edu>
2020-04-18 06:14:42 -0700

    naming bug

--- Compute_Layer/Services/PM/pm.py
+++ Compute_Layer/Services/PM/pm.py
@@ -67,7 +67,7 @@ def get_database_table(table, keys):
 def apply_func_if_not_leaf(value, func):
   if isinstance(value, dict):
     for key, data in value.copy().items():
-      if (apply_func_to_leaves(data)):
+      if (apply_func_if_not_leaf(data)):
         value[key] = func[data]
     return False
   else:
