67bb9536633441515749c65f541765b9bf79af9c
njriasan <njriasanovsky@berkeley.edu>
2020-04-18 01:11:53 -0700

    added usercache to generic streams

--- Compute_Layer/shared_resources/stream_data.py
+++ Compute_Layer/shared_resources/stream_data.py
@@ -1,3 +1,24 @@
+def get_usercache_keys():
+    keys_dict = dict()
+    index1 = ["metadata.write_ts",
+            "metadata.key"]
+    key_one = "metadata.type"
+    for elem in index1:
+        key_one += "\n" + elem
+    keys_dict[key_one] =[["ASCENDING", "ASCENDING", "ASCENDING"], "False"]
+    keys_dict['metadata.write_ts'] = [["DESCENDING"], "False"]
+    keys_dict['data_ts'] = [["DESCENDING"], "True"]
+    return keys_dict
+
+def store_usercache_data(target_address, certificate_path, data):
+    store_data(target_address, "Stage_usercache", get_usercache_keys(), data, certificate_path)
+
+
+def load_usercache_data(target_address, certificate_path, search_fields, 
+        should_sort=False, sort=None):
+    return load_data(target_address, certificate_path, "Stage_usercache", 
+            get_usercache_keys(), search_fields, should_sort, sort)
+
 def get_calendar_keys():
     keys_dict = dict()
     index1 = ["data.attendees", "data.start_time", "data.end_time", "data.ts", "data.geo"]
@@ -9,6 +30,11 @@ def get_calendar_keys():
 
 def store_calendar_data(target_address, certificate_path, data):
     store_data(target_address, "Stage_calendar", get_calendar_keys(), data, certificate_path)
+
+def load_calendar_data(target_address, certificate_path, search_fields, 
+        should_sort=False, sort=None):
+    return load_data(target_address, certificate_path, "Stage_calendar", 
+            get_calendar_keys(), search_fields, should_sort, sort)
 
 def store_data(target_address, certificate_path, data_type, keys, data):
     error = False
@@ -31,8 +57,6 @@ def store_data(target_address, certificate_path, data_type, keys, data):
         print("{} data sucessfully synced to the server".format(data_type))
 
 
-def load_calendar_data(target_address, certificate_path, search_fields, should_sort=False, sort=None):
-    return load_data(target_address, certificate_path, "Stage_calendar", get_calendar_keys(), search_fields, should_sort, sort)
 
 def load_data(target_address, certificate_path, data_type, keys, search_fields, should_sort=False, sort=None):
     error = False
